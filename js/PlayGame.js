e012fa71b8d243092c63c8eae1a8af7b7eb3ea7b
tengwei <312080756@qq.com>
2019-04-08 10:19:13 +0800

    修改

--- js/PlayGame.js
+++ js/PlayGame.js
@@ -1,15 +1,23 @@
 import React,{Component} from "react";
-import {WebView,StyleSheet,View} from "react-native";
+import {WebView,StyleSheet,View,StatusBar} from "react-native";
 
 export default class PlayGame extends Component{
     constructor(props){
         super(props);
+        this.state  = {
+            animated: true,
+            hidden: false,
+            backgroundColor:'white',
+            translucent:false,
+            barStyle:'default',
+            networkActivityIndicatorVisible:false,
+            showHideTransition:'fade',
+        }
     }
 
     static navigationOptions = {
         headerStyle: {
           backgroundColor: '#ffffff',
-          height:0
         },
     }
 
@@ -22,6 +30,15 @@ export default class PlayGame extends Component{
     render(){
         return(
             <View style={styles.container}>
+                <StatusBar
+                    animated={this.state.animated}
+                    hidden={this.state.hidden}
+                    backgroundColor={this.state.backgroundColor}
+                    translucent={this.state.translucent}
+                    barStyle={this.state.barStyle}
+                    networkActivityIndicatorVisible={this.state.networkActivityIndicatorVisible}
+                    showHideTransition={this.state.showHideTransition}
+                />
                 <WebView
                     style={{flex:1}}
                     source={{uri: 'https://sw.gcpstg.m27613.com/luckyjungle/107/index.html?hide_play_for_real=true&startGameToken=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJwbGF5ZXJDb2RlIjoiR29sZFVzZXIxIiwiYnJhbmRJZCI6MTcwMCwiZ2FtZUNvZGUiOiJzd19sdWNreV9qdW5nbGUiLCJwcm92aWRlckNvZGUiOiJTVyIsInByb3ZpZGVyR2FtZUNvZGUiOiJzd19sdWNreV9qdW5nbGUiLCJjdXJyZW5jeSI6IkNOWSIsInBsYXltb2RlIjoiZnVuIiwiZW52SWQiOiJnczEiLCJ0ZXN0IjpmYWxzZSwiaWF0IjoxNTUwNzQ2MjI0LCJleHAiOjE1NTA3NTM0MjQsImlzcyI6InNreXdpbmRncm91cCJ9.8smDWE_ZV-q8j00KeHC1Tkzm95j9uhxK-ZwIIwCTLr_2tAdb73eT-il657yOJI4js0q4Mg-F77SpAOE5VPCVDA&url=https%3A%2F%2Fgs.gcpstg.m27613.com%2Fcasino%2Fgame2&swa=1&history=1&history_url=https%3A%2F%2Fsw.gcpstg.m27613.com%2Fgamehistory%2F3.1.4%2Findex.html&language=zh-cn'}}
